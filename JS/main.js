de781003ddc9b69e309d906a752b104faf17427e
surajitdas5 <124043572+surajitdas5@users.noreply.github.com>
2024-08-05 08:58:56 +0530

    JS Day - 1

---
+++ JS/main.js
@@ -0,0 +1,31 @@
+console.log("Externale JS File");
+
+// Function
+// function test(){
+//     console.log("Test function is called");
+// }
+// test()
+
+// function sumOfTwo(a, b){
+//     console.log(a+b);
+// }
+// sumOfTwo(10, 20)
+
+// function product(a, b){
+//     return a*b;
+// }
+// let c = product(5,4)
+// console.log(c);
+
+console.log(5/2);
+
+// Sum of digit
+n = 123
+let s = 0
+while(n>0){
+    r = n%10
+    s += r
+    n = parseInt(n/10)
+    // console.log(n);
+}
+console.log(s);
